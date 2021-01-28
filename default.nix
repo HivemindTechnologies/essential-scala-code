756a05afaca5ac2ac4b647cfcc21d3185fb50583
hivemind-julius <julius.elias@hivemindtechnologies.com>
2021-01-28 08:54:32 +0100

    remove deprecated compile flag + add extensions to default.nix vscode
    Signed-off-by: hivemind-julius <julius.elias@hivemindtechnologies.com>

--- default.nix
+++ default.nix
@@ -8,7 +8,17 @@ let
     scalameta.metals
     scala-lang.scala
     vscodevim.vim
-  ]);
+  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+      name = "markdown-mermaid";
+      publisher = "bierner";
+      version = "1.9.1";
+      sha256 = "155m0xrdqjg56fbzvfb8ii313qshr55jbpnc1q9kcvw78dcq81b1";
+    } {
+      name = "vscode-mermaid-editor";
+      publisher = "tomoyukim";
+      version = "0.9.2";
+      sha256 = "06ggg0zm5dhmvq46hsi2s6wpgdqzsymh7lpclk2cgbkkw16yz7a7";
+  }];
   vscode-workshop = pkgs.vscode-with-extensions.override {
     vscodeExtensions = vsextensions;
   };
